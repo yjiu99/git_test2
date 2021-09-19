fa5b33c366f7d4db856f95210436e7e3d89214eb
김예지 <yjiu9992@gmail.com>
2021-09-20 03:36:06 +0900

    Hello 클래스 수정

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package test_git2;
 
 public class Main {
     public static void main(String[] args) {
-        Hello h = new Hello();
+        Hello h = new Hello("hello world");
         h.say();
     }
 }
