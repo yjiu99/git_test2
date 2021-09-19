0be9aa90737310e0ccf78b3947e36b45d423d92e
김예지 <yjiu9992@gmail.com>
2021-09-20 03:40:25 +0900

    화면 출력 방법 수정

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ package test_git2;
 
 public class Hello{
   String message = "안녕하세요";
-    public void say(){
-        System.out.println(message);
+    public String say(){
+        return message;
     }
 }
