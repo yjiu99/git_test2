7935a86609693b20fff1c8b2709dedc325e0fafc
김예지 <yjiu9992@gmail.com>
2021-09-20 03:31:39 +0900

    Main.java 수정

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package test_git2;
 
 public class Main {
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
+        Hello hello = new Hello();
+        hello.say();
     }
 }
 
